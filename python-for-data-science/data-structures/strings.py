7f421b92ab3c0a330b577217e7d324272806f04b
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 16:33:45 +0300

    Create strings.py

---
+++ python-for-data-science/data-structures/strings.py
@@ -0,0 +1,21 @@
+##################
+# Strings
+#################
+
+# they are same
+print("John")
+print('John')
+
+"John"
+name = "John"
+long_str = "data structures in python"
+
+# Indexing (Starts with 0)
+name[0] 
+name[3] 
+
+# Slicing
+name[0:2]
+
+# Query
+"data" in long_str # -> True
