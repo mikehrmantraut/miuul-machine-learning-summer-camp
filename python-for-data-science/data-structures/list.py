03e076950ac9d52fe5978322480ede1238469167
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 16:51:18 +0300

    Create list.py

---
+++ python-for-data-science/data-structures/list.py
@@ -0,0 +1,21 @@
+#######################
+# List
+#######################
+
+"""
+Lists are ordered.
+Lists are mutable.
+List elements can be accessed by index.
+"""
+
+notes = [1, 2, 3, 4]
+type(notes)
+
+letters = ["m", "b", "a", "c"]
+not_nam = [1, 2, 3, "a", "b", True, [1, 2, 3]]
+
+not_nam[0]
+not_nam[5]
+not_nam[6][1]
+
+notes[0] = 99
