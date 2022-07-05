d5a9d320ec0bb2ed97d076af373c61ee099bb0ac
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 16:43:12 +0300

    Create string_methods.py

---
+++ python-for-data-science/data-structures/string_methods.py
@@ -0,0 +1,28 @@
+#########################
+# String Methods
+#########################
+
+# len
+name = "john"
+len(name)
+len("baranmunar")
+
+
+# upper() and lower()
+"baran".upper()
+"MUNAR".lower()
+
+# replace
+hi = "Hello AI Era"
+hi.replace("l", "p")
+
+# split
+"Hello AI Era".split()
+
+# strip
+" ofofo ".strip()
+"ofofo".strip("o")
+
+# capitalize
+
+"foo".capitalize()
