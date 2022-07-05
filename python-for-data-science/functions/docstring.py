843e15d6bb3570252949f60ad0126c242b45164d
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 20:08:48 +0300

    Create docstring.py

---
+++ python-for-data-science/functions/docstring.py
@@ -0,0 +1,20 @@
+#########################
+# Docstring
+#########################
+
+def summation(arg1, arg2):
+    """
+    Sum of two numbers.
+
+    Parameters
+    ----------
+    arg1: int, float
+    arg2: int, float
+
+    Returns
+    -------
+    int, float
+
+    
+    """
+    print(arg1 + arg2)
