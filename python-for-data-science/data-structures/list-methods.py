61f12cbec1f0de68d4b5113bc4d6ba406968f26e
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 16:58:08 +0300

    Create list-methods.py

---
+++ python-for-data-science/data-structures/list-methods.py
@@ -0,0 +1,20 @@
+################################
+# List Methods
+################################
+
+notes = [50, 60, 70]
+not_nam = [1, 2, 3, "a", "b", True, [1,2,3]]
+
+# len
+len(notes)
+len(not_nam)
+
+# append
+notes.append(80)
+
+# pop
+notes.pop(0)
+
+# insert
+notes.insert(2, 86)
+print(notes)
