da2f391e2556895a569fa694a57231d35af45cc7
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 11:50:10 +0300

    Create data-structures.py

---
+++ python-for-data-science/data-structures/data-structures.py
@@ -0,0 +1,40 @@
+# Introduction to Data Structures
+
+# Numbers : integer
+x = 46
+type(x)
+
+# Numbers : float
+x = 10.3
+type(x)
+
+# Numbers : complex
+x = 2j + 1
+type(x)
+
+# String
+x = "Hello ai era"
+type(x)
+
+# Boolean
+True
+False
+type(True)
+5 == 4
+type(3 == 2)
+
+# List
+x = ["btc", "eth", "xrp"]
+type(x)
+
+# Dictionary
+x = {"name": "Peter", "Age": 36}
+type(x)
+
+# Tuple
+x = ("python", "ml", "ds")
+type(x)
+
+# Set
+x = {"python", "ml", "ds"}
+type(x)
