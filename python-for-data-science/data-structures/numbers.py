a24c71cb1f1c3b48fcd34a4401ebb55346bdc57f
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-05 16:26:18 +0300

    Create numbers.py

---
+++ python-for-data-science/data-structures/numbers.py
@@ -0,0 +1,31 @@
+## Numbers
+
+a = 5
+b = 10.5
+
+# summation
+a + 5
+a + b
+
+# extraction
+a - 4
+
+# multiplication
+a * 3
+
+# division
+a / 7
+
+# multiple operations
+a * b / 10
+
+# exponentiation
+a ** 2
+a ** 5
+
+# Type Conversions
+int(b)
+float(a)
+
+c = a * b / 10
+int(c)
