bc20725277804e14ad5418165a729646c464206b
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-06 19:23:49 +0300

    Create function-body.py

---
+++ python-for-data-science/functions/function-body.py
@@ -0,0 +1,35 @@
+###################################
+# Function Statements/Body
+###################################
+
+def say_hi():
+    print("Merhaba")
+    print("Hi")
+    print("Hello")
+
+
+say_hi()
+
+def say_hi(string):
+    print(string)
+    print("Hi")
+    print("Hello")
+
+
+say_hi("Merhaba")
+
+def multiplication(a, b):
+    c = a * b
+    print(c)
+
+
+multiplication(10, 9)
+
+list_store = []
+
+def add_elements(a, b):
+    c = a * b
+    list_store.append(c)
+    print(list_store)
+
+add_elements(10, 9)
