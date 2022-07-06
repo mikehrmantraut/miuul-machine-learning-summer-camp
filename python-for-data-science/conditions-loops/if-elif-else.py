f268c5049f0dcc6f03af229716740c197f333c1f
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-06 20:36:46 +0300

    Create if-elif-else.py

---
+++ python-for-data-science/conditions-loops/if-elif-else.py
@@ -0,0 +1,19 @@
+###########################
+# If-Elif-Else Conditions
+###########################
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+    else:
+        print("number is not 10")
+
+number_check(12)
+
+def number_check(number):
+    if number > 10:
+        print("greater than 10")
+    elif number < 10:
+        print("less than 10")
+    else:
+        print("equal to 10")
