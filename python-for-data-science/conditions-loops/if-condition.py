470b6415717dc65d9430ead047ba8f55b1f88c33
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-06 20:34:01 +0300

    Create if-condition.py

---
+++ python-for-data-science/conditions-loops/if-condition.py
@@ -0,0 +1,19 @@
+#######################
+# If Condition
+#######################
+
+# True - False
+1 == 1
+1 == 2
+
+# if 
+if 1 == 1:
+    print("something")
+
+if 1 == 2:
+    print("something")
+
+number = 11
+
+if number == 10:
+    print("number is 10")
