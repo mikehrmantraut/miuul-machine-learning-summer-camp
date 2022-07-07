96553be947cccf43f157c74b9c5ec97e8e1bbfdf
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-07 08:44:18 +0300

    Create break-continue-while.py

---
+++ python-for-data-science/conditions-loops/break-continue-while.py
@@ -0,0 +1,23 @@
+############################
+# While & Break % Continue
+############################
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+# break
+for salary in salaries:
+    if salary == 3000:
+        break
+    print(salary)
+
+# continue
+for salary in salaries:
+    if salary == 3000:
+        continue
+    print(salary)
+
+# while
+number = 1
+while number < 5:
+    print(number)
+    number += 1
