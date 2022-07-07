ca75ed77cdd4ff71565a45d7b2dcd01ed71255cf
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-07 08:48:24 +0300

    Create enumerate.py

---
+++ python-for-data-science/conditions-loops/enumerate.py
@@ -0,0 +1,20 @@
+#########################
+# Enumerate
+#########################
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+for student in students:
+    print(student)
+
+for index, student in enumerate(students):
+    print(index, student)
+
+A = []
+B = []
+
+for index, student in enumerate(students):
+    if index % 2 == 0:
+        A.append(student)
+    else:
+        B.append(student)
