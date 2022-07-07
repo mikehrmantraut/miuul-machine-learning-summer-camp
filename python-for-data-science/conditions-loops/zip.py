e51de2e6c50e33931f86c623eeb83db8fe4cb720
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-07 09:00:33 +0300

    Create zip.py

---
+++ python-for-data-science/conditions-loops/zip.py
@@ -0,0 +1,9 @@
+##################
+# Zip
+##################
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+departmens = ["mathematics", "statistics", "physics", "astronomy"]
+ages = [23, 30, 26, 22]
+
+list(zip(students, departmens, ages))
