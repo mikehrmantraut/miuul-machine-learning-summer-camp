8a314df29804c94824c853ddfb358a9b49149f94
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-07 09:39:24 +0300

    Create comprehension-ex-1.py

---
+++ python-for-data-science/comprehensions/comprehension-ex-1.py
@@ -0,0 +1,28 @@
+#############################################
+# List & Dict Comprehension Exercices - 1
+#############################################
+
+# Changing variable names in a dataset
+
+# before:
+# ['total', 'speeding', 'alcohol', 'not_distracted', 'no_previous', 'ins_premium', 'ins_losses', 'abbrev']
+
+# after:
+# ['TOTAL', 'SPEEDING', 'ALCOHOL', 'NOT_DISTRACTED', 'NO_PREVIOUS', 'INS_PREMIUM', 'INS_LOSSES', 'ABBREV']
+
+import seaborn as sns
+
+df = sns.load_dataset("car_crashes")
+df.columns
+
+for col in df.columns:
+    print(col.upper())
+
+A = []
+for col in df.columns:
+    A.append(col.upper())
+
+df_columns = A
+
+df = sns.load_dataset("car_crashes")
+df_columns = [col.upper() for col in df.columns]
