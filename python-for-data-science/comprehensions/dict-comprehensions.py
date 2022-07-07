cb1fad3977a31a65ad91f972a95c32dff876253e
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-07 09:26:29 +0300

    Create dict-comprehensions.py

---
+++ python-for-data-science/comprehensions/dict-comprehensions.py
@@ -0,0 +1,16 @@
+##############################
+# Dict Comprehensions
+##############################
+
+dictionary = {'a': 1,
+              'b': 2,
+              'c': 3,
+              'd': 4,}
+
+dictionary.keys()
+dictionary.values()
+dictionary.items()
+
+{k: v ** 2 for (k, v) in dictionary.items()}
+
+{k.upper(): v for (k, v) in dictionary.items()}
