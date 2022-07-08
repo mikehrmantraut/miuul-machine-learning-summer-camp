94419698776173e8594ebba48ec5a5bc226a0028
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-08 15:26:18 +0300

    Create read-data.py

---
+++ data-analysis-with-python/pandas/read-data.py
@@ -0,0 +1,11 @@
+#####################
+# Reading Data
+#####################
+
+import pandas as pd
+
+df = pd.read_csv("")
+df = pd.read_excel("")
+df = pd.read_json("")
+df.head(5)
+# pandas cheatsheet
