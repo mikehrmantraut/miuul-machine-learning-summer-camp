811efacc2a727bcbbbb73f1a80b6194b7240e8fd
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-08 13:07:54 +0300

    Create reshape-array.py

---
+++ data-analysis-with-python/numpy/reshape-array.py
@@ -0,0 +1,11 @@
+###################
+# Reshaping
+###################
+
+import numpy as np
+
+np.random.randint(1, 10, size=9)
+np.random.randint(1, 10, size=9).reshape(3, 3)
+
+arr = np.random.randint(1, 10, size=9)
+arr.reshape(3, 3)
