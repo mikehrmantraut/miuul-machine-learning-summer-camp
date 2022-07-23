411af4846a8fa6396e882b1fbc12d9bf23bdcebd
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-23 19:25:04 +0300

    Create 05-local-outlier-factor.py

---
+++ Feature Engineering/Outliers/05-local-outlier-factor.py
@@ -0,0 +1,162 @@
+#########################
+# Accessing Outliers
+#########################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.neighbors import LocalOutlierFactor
+from matplotlib import pyplot as plt
+ 
+def load():
+    data = pd.read_csv("datasets/titanic.csv")
+    return data
+
+df = load()
+df.head()
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == 'O']
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}");
+    print(f"Variables: {dataframe.shape[1]}");
+    print(f"cat_cols: {len(cat_cols)}");
+    print(f"num_cols: {len(num_cols)}");
+    print(f"cat_but_car: {len(cat_but_car)}");
+    print(f"num_but_cat: {len(num_but_cat)}");
+    
+    return cat_cols, num_cols, cat_but_car
+
+
+def outlier_thresholds(dataframe, col_name, q1 = 0.25, q3 = 0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquartile_range = quartile3 - quartile1
+    up_limit = quartile3 + (1.5 * interquartile_range)
+    low_limit = quartile1 - (1.5 * interquartile_range)
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+    
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+    
+grab_outliers(df, "Age")
+
+
+###################################
+# Solving the Outlier Problem
+###################################
+
+# Remove Outlier
+
+low, up = outlier_thresholds(df, "Fare")
+df.shape
+
+df[~((df["Fare"]< low) | (df["Fare"] > up))].shape
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+df.shape
+
+for col in num_cols:
+    new_df = remove_outlier(df, col)
+
+df.shape[0] - new_df.shape[0]
+
+# Re-assignment with thresholds
+
+low, up = outlier_thresholds(df, "Fare")
+
+df[((df["Fare"] < low) | (df["Fare"] > up))]["Fare"]
+
+df.loc[((df["Fare"] < low) | (df["Fare"] > up)), "Fare"]
+
+df.loc[(df["Fare"] > up), "Fare"] = up
+
+df.loc[(df["Fare"] < low), "Fare"] = low
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+df = load()
+df.shape
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+# Local Outlier Factor
+
+df = sns.load_dataset('diamonds')
+df = df.select_dtypes(include=["float64", "int64"])
+df = df.dropna()
+df.head()
+
+for col in df.columns:
+    print(col, check_outlier(df, col))
+
+low, up = outlier_thresholds(df, "carat")
+
+df[((df["carat"] < low) | (df["carat"] > up))].shape
+
+low, up = outlier_thresholds(df, "carat")
+
+df[((df["depth"] < low) | (df["depth"] > up))].shape
+
+clf = LocalOutlierFactor(n_neighbors=20)
+clf.fit_predict(df)
+
+df_scores = clf.negative_outlier_factor_
+df_scores[0:5]
+# df_scores = -df_scores
+
+np.sort(df_scores)[0:5]
+
+scores = pd.DataFrame(np.sort(df_scores))
+scores.plot(stacked=True, xlim=[0,50], style='.-')
+plt.show()
+
+th = np.sort(df_scores)[3]
+
+df[df_scores < th]
+
+df[df_scores < th].shape
+
