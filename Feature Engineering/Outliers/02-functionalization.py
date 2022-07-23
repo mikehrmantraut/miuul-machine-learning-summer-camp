e7900985186abd5dde10234e63399bc0504b2322
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-23 10:16:59 +0300

    Create 02-functionalization.py

---
+++ Feature Engineering/Outliers/02-functionalization.py
@@ -0,0 +1,87 @@
+##################################
+# Functionalization
+##################################
+
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+def load_application_train():
+    data = pd.read_csv("datasets/application_train.csv")
+    return data
+
+df = load_application_train()
+df.head()
+
+def load():
+    data = pd.read_csv("datasets/titanic.csv")
+    return data
+
+df = load()
+df.head()
+
+def outliers_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquartile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquartile_range
+    low_limit = quartile1 - 1.5 * interquartile_range
+    return low_limit, up_limit
+
+outliers_thresholds(df, 'Age')
+outliers_thresholds(df, 'Fare')
+
+low, up = outliers_thresholds(df, 'Fare')
+
+df[(df["Fare"] < low) | (df["Fare"] > up)].head()
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outliers_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+# grab_col_names
+dff = load_application_train()
+df.head()   
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == 'O']
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}");
+    print(f"Variables: {dataframe.shape[1]}");
+    print(f"cat_cols: {len(cat_cols)}");
+    print(f"num_cols: {len(num_cols)}");
+    print(f"cat_but_car: {len(cat_but_car)}");
+    print(f"num_but_cat: {len(num_but_cat)}");
+    
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+cat_cols, num_cols, cat_but_car = grab_col_names(dff)
+
+num_cols = [col for col in num_cols if col not in "SK_ID_CURR"]
+
+for col in num_cols:
+    print(col, check_outlier(dff, col))
