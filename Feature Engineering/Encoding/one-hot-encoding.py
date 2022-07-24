d7b30d28069cf52f916e709e524f47dfc6c2ff73
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-24 17:24:20 +0300

    Create one-hot-encoding.py

---
+++ Feature Engineering/Encoding/one-hot-encoding.py
@@ -0,0 +1,73 @@
+##############################
+# One Hot Encoding
+##############################
+
+from cProfile import label
+from tkinter import Label
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+import missingno as msno
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+# Data set for large scale application
+def load_application_train():
+    data = pd.read_csv("datasets/application_train.csv")
+    return data
+
+
+df_application = load_application_train()
+df_application.head()
+
+# Data set for small scale application
+def load():
+    data = pd.read_csv("datasets/titanic.csv")
+    return data
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == 'O']
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+    return cat_cols, num_cols, cat_but_car
+
+df = load()
+df["Embarked"].value_counts()
+
+pd.get_dummies(df, columns=["Embarked"]).head()
+
+pd.get_dummies(df, columns=["Embarked"], drop_first=True).head()
+
+pd.get_dummies(df, columns=["Embarked"], dummy_na=True).head()
+
+pd.get_dummies(df, columns=["Sex"], drop_first=True).head()
+
+pd.get_dummies(df, columns=["Sex", "Embarked"], drop_first=True).head()
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = load()
+
+
+# cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+
+one_hot_encoder(df, ohe_cols).head()
