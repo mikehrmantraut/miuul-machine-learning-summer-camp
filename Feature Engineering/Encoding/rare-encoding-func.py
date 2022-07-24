7f15edf60a74c920745a8ba049e5e6514fbda8bb
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-24 18:14:30 +0300

    Create rare-encoding-func.py

---
+++ Feature Engineering/Encoding/rare-encoding-func.py
@@ -0,0 +1,82 @@
+##############################
+# Rare Encoding
+##############################
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+# Data set for large scale application
+def load_application_train():
+    data = pd.read_csv("datasets/application_train.csv")
+    return data
+
+
+df_application = load_application_train()
+df_application.head()
+
+# Data set for small scale application
+def load():
+    data = pd.read_csv("datasets/titanic.csv")
+    return data
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == 'O']
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+    return cat_cols, num_cols, cat_but_car
+
+df = load_application_train()
+df["NAME_EDUCATION_TYPE"].value_counts()
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+for col in cat_cols:
+    cat_summary(df,col)
+
+df["NAME_INCOME_TYPE"].value_counts()
+df.groupby("NAME_INCOME_TYPE")["TARGET"].mean()
+
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col,":",len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+rare_analyser(df, "TARGET", cat_cols)
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == 'O'
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis=None)]
+    
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var]  = np.where(temp_df[var].isin(rare_labels), 'Rare', temp_df[var])
+    
+    return temp_df
