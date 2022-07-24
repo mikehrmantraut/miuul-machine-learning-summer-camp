2aba95b507fe7a94db7ff25d51194a9fcaad31e1
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-24 17:30:05 +0300

    Update one-hot-encoding.py

--- Feature Engineering/Encoding/one-hot-encoding.py
+++ Feature Engineering/Encoding/one-hot-encoding.py
@@ -1,16 +1,7 @@
 ##############################
 # One Hot Encoding
 ##############################
-
-from cProfile import label
-from tkinter import Label
-import numpy as np
 import pandas as pd
-import seaborn as sns
-from matplotlib import pyplot as plt
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
-import missingno as msno
-
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
