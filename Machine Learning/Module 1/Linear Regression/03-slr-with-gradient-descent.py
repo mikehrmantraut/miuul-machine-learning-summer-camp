4c5224600f200dacafbd3dd8e69728285271b334
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-07-30 16:13:07 +0300

    Create 03-slr-with-gradient-descent.py

---
+++ Machine Learning/Module 1/Linear Regression/03-slr-with-gradient-descent.py
@@ -0,0 +1,59 @@
+#####################################################################
+# Simple Linear Regression with Gradient Descent from Scratch
+#####################################################################
+
+# Cost Function MSE
+import pandas as pd
+
+def cost_function(Y, b, w, X):
+    m = len(Y)
+    sse = 0
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        sse += (y_hat - y) ** 2
+    mse = sse / m
+    return mse
+
+# Update Weights
+def update_weights(Y, b, w, X, learning_rate):
+    m = len(Y)
+    b_deriv_sum = 0
+    w_deriv_sum = 0
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        b_deriv_sum += (y_hat - y)
+        w_deriv_sum += (y_hat - y) * X[i]
+    new_b = b - (learning_rate * 1 / m * b_deriv_sum)
+    new_w = w - (learning_rate * 1 / m * w_deriv_sum)
+    return new_b, new_w
+
+# Train Function
+def train(Y, initial_b, initial_w, X, learning_rate, num_iters):
+    print("Starting gradient descent at b = {0}, w = {1}, mse = {2}".format(initial_b, initial_w,
+                                                                            cost_function(Y, initial_b, initial_w, X)))
+    b = initial_b
+    w = initial_w
+    cost_history = []
+    for i in range(num_iters):
+        b, w = update_weights(Y, b, w, X, learning_rate)
+        mse = cost_function(Y, b, w, X)
+        cost_history.append(mse)
+        if i % 100 == 0:
+            print("iter={:d} b={:.2f} w={:.4f} mse={:.4}".format(i, b, w, mse))
+    print("After {0} iterations b = {1}, w = {2}, mse = {3}".format(num_iters, b, w, cost_function(Y, b, w, X)))
+    return cost_history, b, w
+
+df = pd.read_csv("datasets/advertising.csv")
+
+X = df["radio"]
+Y = df["sales"]
+
+# hyperparameters
+learning_rate = 0.001
+initial_b = 0.001
+initial_w = 0.001
+num_iters = 10000
+
+train(Y, initial_b, initial_w, X, learning_rate, num_iters)
