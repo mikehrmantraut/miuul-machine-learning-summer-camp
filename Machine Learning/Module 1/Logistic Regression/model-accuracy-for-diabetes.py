cef882122166fcac6c277775310883b948c78324
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-08-01 14:16:15 +0300

    Create model-accuracy-for-diabetes.py

---
+++ Machine Learning/Module 1/Logistic Regression/model-accuracy-for-diabetes.py
@@ -0,0 +1,78 @@
+#################################################
+# Diabetes Prediction with Logistic Regression
+#################################################
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+df = pd.read_csv("datasets/diabetes.csv")
+
+df.groupby("Outcome").agg({"Pregnancies": "mean"})
+
+cols = [col for col in df.columns if "Outcome" not in col]
+
+replace_with_thresholds(df, "Insulin")
+
+for col in cols:
+    df[col] = RobustScaler().fit_transform(df[[col]])
+
+# Model & Prediction
+y = df["Outcome"]
+
+X = df.drop(["Outcome"], axis=1)
+
+log_model = LogisticRegression().fit(X, y)
+
+y_pred = log_model.predict(X)
+
+# Model Evaluation
+def plot_confusion_matrix(y, y_pred):
+    acc = round(accuracy_score(y, y_pred), 2)
+    cm = confusion_matrix(y, y_pred)
+    sns.heatmap(cm, annot=True, fmt=".0f")
+    plt.xlabel('y_pred')
+    plt.ylabel('y')
+    plt.title('Accuracy Score: {0}'.format(acc), size=10)
+    plt.show()
+
+plot_confusion_matrix(y, y_pred)
+
+print(classification_report(y, y_pred))
+
+# Accuracy 0.78
+# Precision 0.74
+# Recall 0.58
+# F1-score 0.65
+
+# ROC AUC
+y_prob = log_model.predict_proba(X)[:, 1]
+roc_auc_score(y, y_prob)
+
+# AUC  0.83939
