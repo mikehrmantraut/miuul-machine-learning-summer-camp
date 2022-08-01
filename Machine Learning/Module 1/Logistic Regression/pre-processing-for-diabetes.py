b71232ec97634eb692e98243cd519cdb55323169
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-08-01 13:57:35 +0300

    Create pre-processing-for-diabetes.py

---
+++ Machine Learning/Module 1/Logistic Regression/pre-processing-for-diabetes.py
@@ -0,0 +1,57 @@
+#################################################
+# Diabetes Prediction with Logistic Regression
+#################################################
+import pandas as pd
+from sklearn.preprocessing import RobustScaler
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+##########################
+# Target vs Features
+##########################
+df = pd.read_csv("datasets/diabetes.csv")
+
+df.groupby("Outcome").agg({"Pregnancies": "mean"})
+
+
+cols = [col for col in df.columns if "Outcome" not in col]
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+for col in cols:
+    target_summary_with_num(df, "Outcome", col)
+
+df.isnull().sum()
+
+for col in cols:
+    print(col, check_outlier(df, col))
+
+replace_with_thresholds(df, "Insulin")
+
+for col in cols:
+    df[col] = RobustScaler().fit_transform(df[[col]])
+
+df.head()
