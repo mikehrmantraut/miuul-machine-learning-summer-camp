437b76620867ddd33fdc858533f6c23a89acb24e
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-08-01 14:08:00 +0300

    Create modelling-for-diabetes.py

---
+++ Machine Learning/Module 1/Logistic Regression/modelling-for-diabetes.py
@@ -0,0 +1,57 @@
+#################################################
+# Diabetes Prediction with Logistic Regression
+#################################################
+
+import pandas as pd
+from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import LogisticRegression
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+df = pd.read_csv("datasets/diabetes.csv")
+
+df.groupby("Outcome").agg({"Pregnancies": "mean"})
+
+cols = [col for col in df.columns if "Outcome" not in col]
+
+replace_with_thresholds(df, "Insulin")
+
+for col in cols:
+    df[col] = RobustScaler().fit_transform(df[[col]])
+
+# Model & Prediction
+y = df["Outcome"]
+
+X = df.drop(["Outcome"], axis=1)
+
+log_model = LogisticRegression().fit(X, y)
+
+log_model.intercept_
+log_model.coef_
+
+y_pred = log_model.predict(X)
+
+y_pred[0:10]
+y[0:10]
